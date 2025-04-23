38debc4f31ad391a12a53bc10d9c821b60f400ed
Snehin Adepu <adepu.snehin@gmail.com>
2025-04-23 14:45:27 +0530

    tomato

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
+            <img src={assets.logo} alt="" className='logo'/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
